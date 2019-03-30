cdf4d5b13dc6c6e3165e5256163e7025f9b0449d
Tom Henighan <henighan.2@gmail.com>
2019-03-30 15:15:44 -0700

    added gaussian policy and tests

---
+++ deeprl/policies/gaussian.py
@@ -0,0 +1,49 @@
+""" Functions for Diagonal Gaussian Policy """
+import tensorflow as tf
+import numpy as np
+
+from deeprl.tf_utils import mlp
+
+
+def mlp_gaussian_policy(x, a, hidden_sizes, activation, action_space,
+                        output_activation=None):
+    """ Builds symbols to sample actions and compute log-probs of actions.  """
+    act_dim = np.prod(action_space.shape)
+    mlp_hidden_sizes = hidden_sizes + (act_dim,)
+    mu = mlp(x, hidden_sizes=mlp_hidden_sizes, activation=activation,
+             output_activation=output_activation)
+    log_std = make_log_std_var(act_dim)
+    pi = sample_actions(mu, log_std)
+    logp = gaussian_likelihood(a, mu, log_std)
+    logp_pi = gaussian_likelihood(pi, mu, log_std)
+    return pi, logp, logp_pi
+
+
+def make_log_std_var(dim):
+    """ Initialize diagonal entries of covariance matrix """
+    init = tf.constant(-0.5, shape=[1, dim], dtype=tf.float32)
+    return tf.get_variable('log_std', initializer=init, trainable=True)
+
+
+def sample_actions(mu, log_std):
+    """ Return a sample from a multivariate normal distribution parameterized
+    by mean mu and log_std, the logs of the diagonals of the covariance matrix
+    """
+    std = tf.exp(log_std)
+    noise = std*tf.random.normal(shape=tf.shape(mu))
+    return mu + noise
+
+
+def gaussian_likelihood(x, mu, log_std):
+    """
+    Args:
+        x: Tensor with shape [batch, dim]
+        mu: Tensor with shape [batch, dim]
+        log_std: Tensor with shape [batch, dim] or [dim]
+    Returns:
+        Tensor with shape [batch]
+    """
+    dim = tf.cast(tf.shape(x)[1], tf.float32)
+    summation_terms = 2*log_std + ((x - mu)**2)/tf.exp(2*log_std)
+    return -0.5*(tf.reduce_sum(summation_terms, axis=1)
+                 + dim*np.log(2*np.pi))
---
+++ tests/policies/test_gaussian.py
@@ -0,0 +1,59 @@
+""" Tests for Gaussian Policy """
+import tensorflow as tf
+import numpy as np
+
+from deeprl.policies import gaussian
+from unittest.mock import Mock
+
+
+class TestGaussian(tf.test.TestCase):
+    """ Gaussian Policy Unittests """
+
+    def test_make_log_std_smoke(self):
+        """ smoke test make_log_std """
+        dim = 12
+        with self.cached_session() as sess:
+            ret = gaussian.make_log_std_var(dim)
+            n_trainable_variables = len(tf.trainable_variables())
+            # ensure log_std is trainable
+            self.assertEqual(n_trainable_variables, 1)
+            sess.run(tf.global_variables_initializer())
+            np.testing.assert_array_equal(ret.eval(), -0.5*np.ones([1, dim]))
+
+
+    def test_sample_actions_smoke(self):
+        """ smoke test sample_actions """
+        batch_size = 5
+        dim = 3
+        mu = np.zeros(shape=[5, dim])
+        log_std = np.zeros(shape=[1, dim])
+        with self.cached_session() as sess:
+            mu_ph = tf.placeholder(dtype=tf.float32, shape=[None, dim])
+            log_std_ph = tf.placeholder(dtype=tf.float32, shape=[1, dim])
+            ret = gaussian.sample_actions(mu_ph, log_std_ph)
+            ret_eval = sess.run(ret, feed_dict={mu_ph: mu, log_std_ph: log_std})
+            self.assertEqual(ret_eval.shape, (batch_size, dim))
+
+    def test_mlp_gaussian_policy_smoke(self):
+        """ smoke test of mlp_gaussian_policy """
+        batch_size = 5
+        obs_dim = 3
+        act_dim = 2
+        action_space = Mock()
+        action_space.contains.return_value = True
+        action_space.shape = (act_dim,)
+        hidden_sizes = (4,)
+        x = np.zeros(shape=[batch_size, obs_dim])
+        a = np.zeros(shape=[batch_size, act_dim])
+        with self.cached_session() as sess:
+            x_ph = tf.placeholder(dtype=tf.float32, shape=[None, obs_dim])
+            a_ph = tf.placeholder(dtype=tf.float32, shape=[None, act_dim])
+            ret = gaussian.mlp_gaussian_policy(
+                x_ph, a_ph, hidden_sizes=hidden_sizes, activation=tf.tanh,
+                output_activation=tf.tanh, action_space=action_space)
+            sess.run(tf.global_variables_initializer())
+            ret_pi, ret_logp, ret_logp_pi = sess.run(
+                ret, feed_dict={x_ph: x, a_ph: a})
+            self.assertEqual(ret_pi.shape, (batch_size, act_dim))
+            self.assertEqual(ret_logp.shape, (batch_size,))
+            self.assertEqual(ret_logp_pi.shape, (batch_size,))
