cf087638c671ac3b6897b437935bc6617591de11
Tom Henighan <henighan.2@gmail.com>
2019-03-30 16:02:34 -0700

    added gaussian_likelihood tests I forgot

--- tests/policies/test_gaussian.py
+++ tests/policies/test_gaussian.py
@@ -57,3 +57,42 @@ class TestGaussian(tf.test.TestCase):
             self.assertEqual(ret_pi.shape, (batch_size, act_dim))
             self.assertEqual(ret_logp.shape, (batch_size,))
             self.assertEqual(ret_logp_pi.shape, (batch_size,))
+
+    def test_gaussian_likelihood_log_std_2d(self):
+        """ test for guassian likelihood when log_std is 2 dimensional """
+        batch_size = 3
+        dim = 2
+        x = tf.constant(0, dtype=tf.float32, shape=[batch_size, dim])
+        mu = tf.constant(0, dtype=tf.float32, shape=[batch_size, dim])
+        log_std = tf.constant(1, dtype=tf.float32, shape=[batch_size, dim])
+        ret = gaussian.gaussian_likelihood(x, mu, log_std)
+        expected = (- dim - 0.5*dim*np.log(2*np.pi))*np.ones([batch_size])
+        with self.cached_session():
+            self.assertAllClose(expected, ret.eval())
+
+    def test_gaussian_likelihood_log_std_1d(self):
+        """ test for guassian likelihood when log_std is 1 dimensional """
+        batch_size = 3
+        dim = 2
+        x = tf.constant(0, dtype=tf.float32, shape=[batch_size, dim])
+        mu = tf.constant(0, dtype=tf.float32, shape=[batch_size, dim])
+        log_std = tf.constant(1, dtype=tf.float32, shape=[dim])
+        ret = gaussian.gaussian_likelihood(x, mu, log_std)
+        expected = (- dim - 0.5*dim*np.log(2*np.pi))*np.ones([batch_size])
+        with self.cached_session():
+            self.assertAllClose(expected, ret.eval())
+
+    def test_gaussian_likelihood_non_zero_difference(self):
+        """ test for guassian likelihood when log_std is 1 dimensional """
+        batch_size = 3
+        dim = 2
+        x = tf.constant(2, dtype=tf.float32, shape=[batch_size, dim])
+        mu = tf.constant(0, dtype=tf.float32, shape=[batch_size, dim])
+        log_std = tf.constant(1, dtype=tf.float32, shape=[dim])
+        ret = gaussian.gaussian_likelihood(x, mu, log_std)
+        expected_summation_term = -0.5*dim*(4/np.e**2 + 2)
+        expected_scalar_term = -0.5*dim*np.log(2*np.pi)
+        expected = (expected_summation_term
+                    + expected_scalar_term)*np.ones([batch_size])
+        with self.cached_session():
+            self.assertAllClose(expected, ret.eval())
