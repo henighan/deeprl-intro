0d58babdf3aa520efd42fea91bdf5b3eae868212
Tom Henighan <henighan.2@gmail.com>
2019-03-31 09:00:27 -0700

    made updates to vpg agent and added tests

--- deeprl/agents/vpg.py
+++ deeprl/agents/vpg.py
@@ -7,11 +7,12 @@ from deeprl.tf_utils import mlp
 
 class VPG():
 
-    def __init__(self, pi_lr=1e-3, val_lr=1e-3, obs_space=None,
-                 act_space=None, hidden_sizes=(32, 32),
-                 val_train_iters=80, sess=None, close_sess=True):
+    def __init__(self, pi_lr=1e-3, val_lr=1e-3, hidden_sizes=(32, 32),
+                 activation=tf.tanh, val_train_iters=80, sess=None,
+                 close_sess=True):
         self.pi_lr, self.val_lr = pi_lr, val_lr
         self.hidden_sizes = hidden_sizes
+        self.activation = activation
         self.val_train_iters = val_train_iters
         self.sess = sess or tf.Session()
         self.close_sess = close_sess
@@ -20,8 +21,8 @@ class VPG():
     def step(self, obs):
         """ sample action given observation of environment """
         feed_dict = {self.placeholders['obs']: obs.reshape(1, -1)}
-        act, val_t, logp_t = self.sess.run((self.pi, self.val, self.logp_pi),
-                                            feed_dict=feed_dict)
+        act, val_t, logp_t = self.sess.run(
+            (self.pi, self.val, self.logp_pi), feed_dict=feed_dict)
         return act[0], val_t[0], logp_t[0]
 
     def train(self, obs_buf, act_buf, adv_buf, ret_buf, logp_buf):
@@ -41,7 +42,7 @@ class VPG():
         # calculate change in loss
         new_pi_loss, new_val_loss = self.sess.run(
             (self.pi_loss, self.val_loss), feed_dict=feed_dict)
-        delta_pi_loss  = new_pi_loss - old_pi_loss
+        delta_pi_loss = new_pi_loss - old_pi_loss
         delta_val_loss = new_val_loss - old_val_loss
         return old_pi_loss, old_val_loss, delta_pi_loss, delta_val_loss
 
@@ -50,15 +51,18 @@ class VPG():
         """ sampled action, logprob of input action, and logprob of
         sampled action """
         self.pi, self.logp, self.logp_pi = self.build_policy(
-            act_space, self.placeholders['obs'], self.act_ph)
+            act_space, self.placeholders['obs'], self.placeholders['act'],
+            self.hidden_sizes, self.activation)
         # policy loss and train op
         self.pi_loss, self.pi_train_op = self.build_policy_loss(
-            self.logp, self.placeholders['adv'])
+            self.logp, self.placeholders['adv'], self.pi_lr)
         # value function estimator
-        self.val = self.build_value_function(self.placeholders['obs'])
+        self.val = self.build_value_function(self.placeholders['obs'],
+                                             hidden_sizes=self.hidden_sizes,
+                                             activation=self.activation)
         # value function estimator loss and train op
         self.val_loss, self.val_train_op = self.build_val_loss(
-            self.val, self.placeholders['ret'])
+            self.val, self.placeholders['ret'], lr=self.val_lr)
 
     def create_placeholders(self, obs_space, act_space):
         """ Build the placeholders required for this agent """
@@ -70,32 +74,37 @@ class VPG():
             self.placeholders[name] = tf.placeholder(
                 dtype=tf.float32, shape=[None], name=name)
 
-    def build_value_function(self, obs_ph):
+    @staticmethod
+    def build_value_function(obs_ph, hidden_sizes, activation):
         with tf.variable_scope('val'):
-            val = mlp(obs_ph, hidden_sizes=self.hidden_sizes + (1,),
-                      activation=tf.tanh)
-        return val
+            val = mlp(obs_ph, hidden_sizes=hidden_sizes + (1,),
+                      activation=activation)
+        return tf.squeeze(val)
 
-    def build_policy(self, act_space, obs_ph, act_ph):
+    @staticmethod
+    def build_policy(act_space, obs_ph, act_ph, hidden_sizes, activation):
         if isinstance(act_space, Box):
             with tf.variable_scope('policy'):
                 pi, logp, logp_pi = mlp_gaussian_policy(
-                    self.placeholders['obs'], self.act_ph, hidden_sizes=(32, 32),
-                    activation=tf.tanh, action_space=act_space)
+                    obs_ph, act_ph, hidden_sizes=hidden_sizes,
+                    activation=activation, action_space=act_space)
             return pi, logp, logp_pi
         raise NotImplementedError(
             "Only Policies for Box action space implemented")
 
-    def build_policy_loss(self, logp, adv):
-        pi_loss = -logp*adv
+    @staticmethod
+    def build_policy_loss(logp, adv_ph, lr):
+        pi_loss = tf.reduce_mean(-logp*adv_ph)
         pi_train_op = tf.train.AdamOptimizer(
-            learning_rate=self.pi_lr).minimize(pi_loss)
+            learning_rate=lr).minimize(pi_loss)
         return pi_loss, pi_train_op
 
-    def build_val_loss(self, val, ret_ph):
+    @staticmethod
+    def build_val_loss(val, ret_ph, lr):
         val_loss = tf.losses.mean_squared_error(val, ret_ph)
-        self.val_train_op = tf.train.AdamOptimizer(
-            learning_rate=self.val_lr).minimize(self.val_loss)
+        val_train_op = tf.train.AdamOptimizer(
+            learning_rate=lr).minimize(val_loss)
+        return val_loss, val_train_op
 
     # def __del__(self):
     #     if self.sess and self.close_sess:
---
+++ tests/agents/test_vpg.py
@@ -0,0 +1,84 @@
+""" Tests for vpg agent """
+import numpy as np
+import tensorflow as tf
+import gym
+
+from deeprl.agents.vpg import VPG
+from deeprl import tf_utils
+from deeprl.tf_utils import tfph
+
+
+class TestVPG(tf.test.TestCase):
+    """ Tests for VPG Agent """
+
+    def setUp(self):
+        super().setUp()
+        self.env = gym.make('MountainCarContinuous-v0')
+        self.vpg = VPG(hidden_sizes=(4,))
+
+    def test_create_placeholders_smoke(self):
+        """ smoke test placeholder creation """
+        self.vpg.create_placeholders(self.env.observation_space,
+                                     self.env.action_space)
+        self.assertSetEqual(set(list(self.vpg.placeholders.keys())),
+                            {'obs', 'ret', 'adv', 'act'})
+
+    def test_build_value_function_smoke(self):
+        """ check the num of trainable params and output shape make sense for
+        build_value_func """
+        obs_dim = self.env.observation_space.shape[0]
+        obs_ph = tf_utils.tfph(obs_dim)
+        obs = np.random.rand(8, obs_dim)
+        val = self.vpg.build_value_function(
+            obs_ph, hidden_sizes=(4,), activation=None)
+        n_params = (obs_dim+1)*4 + (4 + 1)*1
+        with self.cached_session() as sess:
+            ret_n_params = sess.run(tf_utils.trainable_count(scope='val'))
+            sess.run(tf.global_variables_initializer())
+            sess_val = sess.run(val, feed_dict={obs_ph: obs})
+        self.assertEqual(n_params, ret_n_params)
+        self.assertEqual(sess_val.shape, (8,))
+
+    def test_build_val_loss_smoke(self):
+        """ Make sure the loss goes down when training, and that training
+        brings val closer to rets """
+        batch_size = 4
+        ret_ph = tf_utils.tfph(None)
+        ret = np.ones(batch_size)
+        val = tf.get_variable(
+            'val', dtype=tf.float32, trainable=True,
+            initializer=4*[0.])
+        loss, train_op = self.vpg.build_val_loss(val, ret_ph, 1e-3)
+        with self.cached_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            old_loss = sess.run(loss, feed_dict={ret_ph: ret})
+            sess.run(train_op, feed_dict={ret_ph: ret})
+            new_loss = sess.run(loss, feed_dict={ret_ph: ret})
+            new_val = sess.run(val)
+        self.assertEqual(new_loss.shape, tuple())
+        self.assertLess(new_loss, old_loss)
+        self.assertTrue(all(new_val > 0))
+
+    def test_build_policy_loss_smoke(self):
+        """ Make sure the loss goes down when training, and that training
+        changes logp in the expected direction """
+        batch_size = 4
+        adv_ph = tfph(None)
+        adv = np.ones(batch_size)
+        logp = tf.get_variable(
+            'adv', dtype=tf.float32, trainable=True,
+            initializer=batch_size*[0.])
+        loss, train_op = self.vpg.build_policy_loss(logp, adv_ph, lr=1e-3)
+        with self.cached_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            old_loss = sess.run(loss, feed_dict={adv_ph: adv})
+            sess.run(train_op, feed_dict={adv_ph: adv})
+            new_loss = sess.run(loss, feed_dict={adv_ph: adv})
+            new_logp = sess.run(logp)
+        self.assertEqual(new_loss.shape, tuple())
+        self.assertLess(new_loss, old_loss)
+        self.assertTrue(all(new_logp > 0))
+
+    def test_build_graph_smoke(self):
+        """ Smoke test for building the graph """
+        self.vpg.build_graph(self.env.observation_space, self.env.action_space)
