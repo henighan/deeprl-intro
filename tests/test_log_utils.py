d646bed9eb2a656913e58eaf822257de483fd9e2
Tom Henighan <henighan.2@gmail.com>
2019-03-31 17:41:06 -0700

    fixed formatting in log utils tests

--- tests/test_log_utils.py
+++ tests/test_log_utils.py
@@ -3,17 +3,22 @@ import tensorflow as tf
 
 from deeprl import log_utils
 
+
 def test_hidden_sizes_to_str_smoke():
     """ smoke test converting hidden sizes to str """
     assert '32' == log_utils.hidden_sizes_to_str((32,))
     assert '32-64' == log_utils.hidden_sizes_to_str((32, 64))
 
+
 def test_kwargs_to_exp_name_strs():
+    """ test converting kwargs to strings for exp name """
     kwargs = {'num_runs': 3, 'activation': tf.tanh, 'hidden_sizes': (32, 64)}
     ret = log_utils.kwargs_to_exp_name_strs(kwargs)
     assert ret == ['tanh', 'hid32-64']
 
+
 def test_kwargs_to_exp_name():
+    """ test converting kwargs to exp name """
     ret = log_utils.kwargs_to_exp_name('test_exp', 0, {'hidden_sizes': (32,)})
     assert ret == 'test_exp_hid32_s0'
     ret = log_utils.kwargs_to_exp_name('test_exp', 0, {})
