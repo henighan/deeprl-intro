e1a893501c17238eb6d5726864d90db1da79df9a
Tom Henighan <henighan.2@gmail.com>
2019-04-01 17:44:21 -0700

    first pass at plotting function

--- deeprl/tasks/plotting.py
+++ deeprl/tasks/plotting.py
@@ -1,17 +1,27 @@
 """ Plotters """
 import os
-import glob
+
 from matplotlib import pyplot as plt
-from deeprl import log_utils
 import pandas as pd
 
-def plot(exp_name, implementation, **kwargs):
-    output_dir_base = log_utils.output_dir_base_from_kwargs(
-        exp_name, implementation, kwargs)
-    dfs = {}
-    for seed_dir in glob.glob(output_dir_base + "_s*"):
-        seed = int(seed_dir.split('s')[-1])
-        dfs[seed] = pd.read_csv(
-            os.path.join(seed_dir, 'progress.txt'), delimiter='\t')
-    dfs[0]['AverageEpRet'].plot()
+from deeprl import log_utils
+
+
+def single_plot(exp_name, num_runs, implementation,
+                value='AverageEpRet', epochs=None, **kwargs):
+    """ Plot results for a single implementation """
+    data = pd.DataFrame()
+    for run_no in range(num_runs):
+        seed = 10*run_no
+        output_dir = log_utils.output_dir_from_kwargs(
+            exp_name, implementation, kwargs, seed=seed)
+        path = os.path.join(output_dir, 'progress.txt')
+        tmp_df = pd.read_csv(path, delimiter='\t')[[
+            value, 'Epoch', 'TotalEnvInteracts']]
+        if epochs:
+            tmp_df = tmp_df[tmp_df['Epoch'] < epochs]
+        data = pd.concat([tmp_df, data])
+    plt.plot(data['TotalEnvInteracts'], data[value], 'o')
+    plt.xlabel('TotalEnvInteracts')
+    plt.ylabel(value)
     plt.show()
