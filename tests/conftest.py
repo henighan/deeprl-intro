d903cf0a715394a847418f19fe729776bb5ca888
Tom Henighan <henighan.2@gmail.com>
2019-04-27 21:01:47 -0700

    moved policy_gradient learner pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,13 +10,3 @@ def discrete_env():
 @pytest.fixture
 def continuous_env():
     return gym.make('MountainCarContinuous-v0') 
-
-@pytest.fixture
-def learner(mocker, continuous_env):
-    learner_path = 'deeprl.learners.policy_gradient_learner.'
-    mocker.patch(learner_path + 'EpochLogger.save_config')
-    mocker.patch(learner_path + 'EpochLogger.log')
-    mocker.patch(learner_path + 'EpochLogger.setup_tf_saver')
-    agent = mocker.Mock()
-    agent.build_graph.return_value = {'foo': 'bar'}
-    return PolicyGradientLearner(agent, env=continuous_env)
--- tests/learners/test_policy_gradient.py
+++ tests/learners/test_policy_gradient.py
@@ -10,6 +10,17 @@ from deeprl.learners import PolicyGradientLearner
 
 
 @pytest.fixture
+def learner(mocker, continuous_env):
+    learner_path = 'deeprl.learners.policy_gradient_learner.'
+    mocker.patch(learner_path + 'EpochLogger.save_config')
+    mocker.patch(learner_path + 'EpochLogger.log')
+    mocker.patch(learner_path + 'EpochLogger.setup_tf_saver')
+    agent = mocker.Mock()
+    agent.build_graph.return_value = {'foo': 'bar'}
+    return PolicyGradientLearner(agent, env=continuous_env)
+
+
+@pytest.fixture
 def agent_step_ret():
     return ({'act': np.array([0]), 'val': 0, 'logp': 0},
             {'VVals': 0, 'Logp': 0})
